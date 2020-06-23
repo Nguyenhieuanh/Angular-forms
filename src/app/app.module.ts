40b2b42a6c843a9f1c8c41a0c001546646826e0c
Nguyenhieuanh <nguyenhieuanh.061626@gmail.com>
2020-06-23 18:54:30 +0700

    register form

---
+++ src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { LoginFormComponent } from './components/login-form/login-form.component';
+import { RegisterFormComponent } from './components/register-form/register-form.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginFormComponent,
+    RegisterFormComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/components/register-form/register-form.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { MustMatch } from '../../_helpers/must-match.validator';
+
+
+@Component({
+  selector: 'app-register-form',
+  templateUrl: './register-form.component.html',
+  styleUrls: ['./register-form.component.css']
+})
+export class RegisterFormComponent implements OnInit {
+  registerForm: FormGroup;
+  submitted = false;
+
+  constructor(private formBuilder: FormBuilder) { }
+
+  ngOnInit(): void {
+    this.registerForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(8), Validators.maxLength(20)]],
+      passwordConfirm: ['', Validators.required],
+      country: ['Ha Noi', Validators.required],
+      age: [null, [Validators.required, Validators.min(18)]],
+      gender: ['Male', Validators.required],
+      phone: ['', [Validators.required, Validators.pattern(/^\+84\d{9,10}$/)]],
+    }, {
+      validator: MustMatch('password', 'passwordConfirm')
+    });
+  }
+
+  get f() {
+    return this.registerForm.controls;
+  }
+
+  onSubmit() {
+    this.submitted = true;
+    console.log(this.registerForm);
+  }
+
+}
