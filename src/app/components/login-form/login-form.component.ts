470e29173ae1dd33847f2526aecdf1e4f1707892
Nguyenhieuanh <nguyenhieuanh.061626@gmail.com>
2020-06-23 21:32:11 +0700

    class login component

--- src/app/components/login-form/login-form.component.ts
+++ src/app/components/login-form/login-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login-form',
@@ -6,10 +7,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login-form.component.css']
 })
 export class LoginFormComponent implements OnInit {
+  loginForm: FormGroup;
+  submitted = false;
+  msg = null;
 
-  constructor() { }
+  private email = 'admin@example.com';
+  private password = 'password';
+
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
+    this.loginForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]]
+    });
   }
 
+  onSubmit() {
+    if (this.loginForm.value.email === this.email
+      && this.loginForm.value.password === this.password) {
+      this.submitted = true;
+      alert('Welcome!');
+    } else {
+      this.msg = 'Incorrect email or password.';
+    }
+  }
 }
